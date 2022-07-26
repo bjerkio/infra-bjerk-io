fd773edde55966b1fec1c1ca837f0e36fec70d8f
Simen A. W. Olsen <so@bjerk.io>
2022-07-26 17:40:43 +0200

    chore: add k8s cluster

--- src/zones/bjerk-dev.ts
+++ src/zones/bjerk-dev.ts
@@ -1,6 +1,6 @@
 import * as gcp from '@pulumi/gcp';
 
-export const managedZone = new gcp.dns.ManagedZone(
+export const zone = new gcp.dns.ManagedZone(
   'bjerk-dev-zone',
   {
     name: 'bjerk-dev',
@@ -13,3 +13,31 @@ export const managedZone = new gcp.dns.ManagedZone(
   { protect: true },
 );
 
+
+const managedZone = zone.name;
+const ttl = 300;
+const opts = { protect: true };
+
+new gcp.dns.RecordSet(
+  'k8s01-bjerk-dev-a',
+  {
+    managedZone,
+    name: 'k8s01.bjerk.io.',
+    rrdatas: ['185.243.218.58'],
+    ttl,
+    type: 'A',
+  },
+  opts,
+);
+
+new gcp.dns.RecordSet(
+  'k8s01-bjerk-dev-aaaa',
+  {
+    managedZone,
+    name: 'k8s01.bjerk.io.',
+    rrdatas: ['2a03:94e0:ffff:185:243:218::58'],
+    ttl,
+    type: 'AAAA',
+  },
+  opts,
+);
